b078910ea48b584c16523525c7da77febffc62ca
AndreasElgaard <andreas.elgaard@outlook.com>
2021-05-12 15:29:20 +0200

    submission services

---
+++ Heat/TECAIS.HeatSubmissionService/TECAIS.HeatSubmissionService/Config/RabbitMQ.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TECAIS.HeatSubmissionService.Config
+{
+    public class RabbitMq
+    {
+        public const string RabbitMqUri = "localhost"; //"rabbitmq://localhost/heatsubmission/";
+        public const string UserName = "guest";
+        public const string Password = "guest";
+    }
+}
